e6d997c399dfb2a72159b2a6296da753ce5c072e
ASHUKUMARTHAKUR <https://github.com/ASHUKUMARTHAKUR/HTML-1.git>
2026-03-11 10:30:39 +0530

    script2

---
+++ script2.js
@@ -0,0 +1,10 @@
+var person2 = new Object();
+person2.name = "John";
+person2.age = 30;
+person2.sMarried = true;
+person2.address = []
+person2.address.street = "123 Main St";
+person2.address.city = "New York";
+person2.address.state = "NY";
+person2.address.zip = "10001";
+console.log("person2.name: " + person2.name);
