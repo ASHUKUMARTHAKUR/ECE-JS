341fd84fd8e4b24a95f3438661384d077d199bc0
ASHUKUMARTHAKUR <https://github.com/ASHUKUMARTHAKUR/HTML-1.git>
2026-03-11 09:23:41 +0530

    my 2nd commit

---
+++ scrpt1.js
@@ -0,0 +1,11 @@
+for(key in object){
+    //code to be executed
+}
+const person = {
+    fname : "Ashu",
+    lname : "Thakur",
+    age : 19
+};
+for(let x in person){
+    console.log("person.details: " + x + ":"+ person[x]);
+}
