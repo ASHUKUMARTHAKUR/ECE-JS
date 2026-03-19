3d10dc7e0ecce8c107e2df823c71ba3520f50550
ASHUKUMARTHAKUR <https://github.com/ASHUKUMARTHAKUR/HTML-1.git>
2026-03-19 11:38:55 +0530

    Events

---
+++ script5.js
@@ -0,0 +1,28 @@
+function onClick() {
+    alert('Button clicked (onclick)');
+}
+
+function myMouseDown() {
+    alert('Mouse button pressed (onmousedown)');
+}
+
+function myMouseUp() {
+    alert('Mouse button released (onmouseup)');
+}
+
+function myMouseOver() {
+    alert('Mouse is over the button (onmouseover)');
+}
+
+function myMouseOut() {
+    alert('Mouse left the button (onmouseout)');
+}
+
+function myMouseMove() {
+    alert('Mouse moved over the button (onmousemove)');
+}
+
+function myDoubleClick() {
+    alert('Button double-clicked (ondblclick)');
+}
+
